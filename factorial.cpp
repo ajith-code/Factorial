7e638c16c47d8658d98fdaca3bd3b8fcc12f328d
ajith-code <70834458+ajith-code@users.noreply.github.com>
2021-05-18 20:43:14 +0530

    Add files via upload
    Factorial the number less than 20

---
+++ factorial.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+
+{
+    
+int a,mul=1;
+    
+cout<<"Enter the factorial number:";
+    
+cin>>a;
+    
+if(a==0)
+{
+        
+cout<<"factorial is:"<<a;
+        
+    
+}
+    
+for(int i=a;i>=1;i--)
+    
+{
+      
+mul=mul*i;  
+    
+}
+    
+cout<<"factorial is:"<<mul;
+    
+return 0;
+
+}
